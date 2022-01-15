640616cd3c5b5812f5f3926dce28a472017bea41
cjmcguire88 <cjmcguire88@gmail.com>
2022-01-15 14:40:11 -0500

    Combined multiple uses of echo

--- kernel/assets/k_info.sh
+++ kernel/assets/k_info.sh
@@ -29,13 +29,13 @@ KERNELS=$(ls "$KERNEL_DIR"/vmlinuz-linux*)
 BACKUPS=$(ls "$SRC_DIR"/backups/*.tar.gz)
 UKI=$(ls "$KERNEL_DIR"/*.efi)
 
-echo -e "\033[1;37mKernel version: \033[1;32mlinux-$(uname -r)\033[0m"
-echo -e "\033[1;37mKernel source directory:\033[0m $SRC_DIR/linux-$(uname -r)/"
-echo -e "\033[1;37mCompiled on: \033[0m$M_TIME\n"
-echo -e "\033[1;37mPatches applied:\033[0m\n$(ls "$SRC_DIR"/linux-"$(uname -r)"/patches/)\n"
-echo -e "\033[1;37mInstalled kernels \033[0m($KERNEL_DIR)\033[1;37m:\033[0m"
+echo -e "\033[1;37mKernel version: \033[1;32mlinux-$(uname -r)\033[0m
+\033[1;37mKernel source directory:\033[0m $SRC_DIR/linux-$(uname -r)/
+\033[1;37mCompiled on: \033[0m$M_TIME\n
+\033[1;37mPatches applied:\033[0m\n$(ls "$SRC_DIR"/linux-"$(uname -r)"/patches/)\n
+\033[1;37mInstalled kernels \033[0m($KERNEL_DIR)\033[1;37m:\033[0m"
 printf '%s\n' "${KERNELS//$KERNEL_DIR\/vmlinuz-}"
-if [[ -n $(ls "$KERNEL_DIR"/*.efi) ]]; then
+if [[ -n $UKI ]]; then
     echo -e "\n\033[1;37mUnified Kernel Images \033[0m($KERNEL_DIR)\033[1;37m:\033[0m"
     printf '%s\n' "${UKI//$KERNEL_DIR\/}"
 fi
