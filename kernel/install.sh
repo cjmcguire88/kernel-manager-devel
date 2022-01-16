bb9cb758aa9fa408fdb566d2bf7f82d94b9dce54
cjmcguire88 <cjmcguire88@gmail.com>
2022-01-16 16:02:39 -0500

    Template made for install.sh

---
+++ kernel/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#%    ./install.sh
+#%
+#% DESCRIPTION
+#% Install script for custom-kernel-manager.
+#%
+#% OPTIONS
+#% None
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         custom-kernel-manager 1.0
+#-    author          Jason McGuire
+#-    email           haximus84@protonmail.com
+#-    copyright       None
+#-    license         MIT
+#-
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+
