b6bc42a31ba7fdd6b825fef8661039625534f463
cjmcguire88 <cjmcguire88@gmail.com>
2022-01-16 17:33:23 -0500

    Made options neater and more concise

--- kernel/assets/k_help.sh
+++ kernel/assets/k_help.sh
@@ -25,28 +25,39 @@
 
 cat <<EOF
 Usage: kernel [-flag] [OPTIONAL_ARG]
+Custom Kernel Manager.
 Author: Jason McGuire
-Custom kernel maintenance.
 
--d)   Downloads specified kernel version to $BUILD_DIR (Requires kernel version as argument.)
+-d) Downloads specified kernel version to $BUILD_DIR (Requires kernel version as argument.)
+    Ex. kernel -d 5.15.5
 
--i)   Download, compile and install the kernel version passed as an argument.
+-i) Download, compile and install the kernel version passed as an argument.
+    Ex. kernel -i 5.15.5
 
--b)   Create a .tar.gz archive of the kernel source directory. (Requires kernel version as argument.)
+-b) Create a .tar.gz archive of the kernel source directory. (Requires kernel version as argument.)
+    Ex. kernel -b 5.15.5-NAME
 
--m)   Modify kernel config and optionally recompile and install kernel (Requires kernel version as argument.)
+-m) Modify kernel config and optionally recompile and install kernel (Requires kernel version as argument.)
+    Ex. kernel -m 5.15.5-NAME
 
--r)   Remove a kernel from system. (Requires kernel version as argument.)
+-r) Remove a kernel from system. (Requires kernel version as argument.)
+    Ex. kernel -r 5.15.5-NAME
 
--a)   Restore a kernel that was previously archived. (Requires kernel version as argument.)
+-a) Restore a kernel that was previously archived. (Requires kernel version as argument.)
+    Ex. kernel -a 5.15.5-NAME
 
--c)   View the kernel changelog for the version passed as an argument.
+-c) View the kernel changelog for the version passed as an argument.
+    Ex. kernel -c 5.15.5
 
--p)   Dump a directory containing the patches listed in patchfile given (used for testing).
+-p) Dump a directory containing the patches listed in patchfile given (used for testing).
+    Ex. kernel -p
 
--u)   Update the current kernel to the latest stable on kernel.org.
+-u) Update the current kernel to the latest stable on kernel.org.
+    Ex. kernel -u
 
--n)   Create a new kernel. Select from a menu of the newest kernels on kernel.org
+-n) Create a new kernel. Select from a menu of the newest kernels on kernel.org
+    Ex. kernel -n
 
--h)   Show this dialogue.
+-h) Show this dialogue.
+    Ex. kernel -h
 EOF
