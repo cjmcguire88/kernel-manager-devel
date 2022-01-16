2ce94cdf42848221afb9087555ab271f9b77475b
cjmcguire88 <cjmcguire88@gmail.com>
2022-01-16 16:25:23 -0500

    Option to install regardless if changes are present

--- kernel/assets/k_modify.sh
+++ kernel/assets/k_modify.sh
@@ -30,7 +30,7 @@
 #================================================================
 
 [[ "$1" =~ $(uname -r) ]] && exoe "${1} is the currently running kernel"
-cp -rv "$SRC_DIR"/linux-"$1" "$BUILD_DIR"/
+[[ ! -d $BUILD_DIR/linux-$1 ]] && cp -rv "$SRC_DIR"/linux-"$1" "$BUILD_DIR"/
 cd "$BUILD_DIR"/linux-"${1}" || exoe "${1} not found"
 read -n 1 -p $'\033[1;37mCreate backup of kernel source? \033[0m[y/N]: ' REPLY
 if [[ ${REPLY:-N} =~ ^[Yy]$ ]]; then
@@ -42,13 +42,14 @@ newSum=$(md5sum .config)
 if [[ $oldSum != "$newSum" ]]; then
     [[ ! -d "$HOME"/.config/kernel/configs/"$(date +"%Y-%m-%d")" ]] && mkdir -p "$HOME"/.config/kernel/configs/"$(date +"%Y-%m-%d")"
     diff .config.old .config > "$HOME"/.config/kernel/configs/"$(date +"%Y-%m-%d")"/"${1}-$(date | awk '{print $4}')".diff
-    read -n 1 -p $'\n\033[1;37mRecompile and install? \033[0m[y/N]: ' REPLY
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        make clean
-        make -j$(($(nproc) - $(nproc) / 4)) || exoe "Compilation failed"
-        sudo -i "$k_path"/assets/k_install.sh "${1}"
-    fi
 else
-    echo -e "\033[1;37mNo changes were made. Exiting\033[0m"
-    exit
+    echo -e "\033[1;37mNo changes were made.\033[0m"
+fi
+read -n 1 -p $'\n\033[1;37mRecompile and install? \033[0m[y/N]: ' REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    make clean
+    make -j$(($(nproc) - $(nproc) / 4)) || exoe "Compilation failed"
+    sudo "$k_path"/assets/k_install.sh "${1}" "$BUILD_DIR"
+else
+    exit 0
 fi
